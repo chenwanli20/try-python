7c64187d25c976c0631d8ac841d178fb0f402ef0
陈万里 <wanl.chen@hnair.com>
2018-08-02 13:41:14 +0800

    five

--- One/www/orm.py
+++ One/www/orm.py
@@ -2,6 +2,12 @@ import asyncio, logging
 
 import aiomysql
 
+
+def log（sql, args=()）:
+    logging.info('SQL: %s' % sql)
+
+
+    
 async def create_pool(loop, **kw):
     logging.info('create database connection pool...')
     global __pool
@@ -95,5 +101,41 @@ class ModelMetaclass(type):
             return type.__new__(cls, name, bases, attrs)
 
         tableName = attrs.get('__table__', None) or name
+        logging.info('found model: %s (table: %s)' % (name, tableName))
+        mappings = dict()
+        fields = []
+        primaryKey = None
+        
+        for k, v in attrs.items():
+            if isinstance(v, Field):
+                logging.info('  found mapping: %s ==> %s' % (k, v))
+                mappings[k] = v
+                if v.primary_key:
+                    if primaryKey:
+                        raise StandardError('Duplicate primary key for field: %s' % k)
+                    primaryKey = k
+                else:
+                    fields.append(k)
+
+        if not primaryKey:
+            raise StandardError('Primary key not found.')
+        for k in mappings.keys():
+            attrs.pop(k) 
+
+        escaped_fields = list(map(lambda f: '`%s`' % f, fields))
+
+        attrs['__mappings__'] = mappings        #保存属性和列的映射关系
+        attrs['__table__'] = tableName
+        attrs['__primary_key__'] = primaryKey   #主键属性名
+        attrs['__fields__'] = fields            #除主键外的属性名
+        attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ','.join(escaped_fields), tableName)
+        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
+        attrs['__update__'] = 'update `%s` set %s where `%s` = ?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
+        attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
+
+        return type.__new__(cls, name, bases, attrs)
+
+        
+
 
 
